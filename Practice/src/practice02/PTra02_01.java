6e413105fa406f309d7dbce433801e6c32e09420
user-PC\user <believe.3785@gmail.com>
2018-06-19 13:46:37 +0900

    2-10

--- Practice/src/practice02/PTra02_01.java
+++ Practice/src/practice02/PTra02_01.java
@@ -10,12 +10,21 @@ public class PTra02_01 {
 	public static void main(String[] args) {
 
 		// ★ 20を5で割った値を出力してください
+		int a;
+		a=20/5;
+		System.out.println(a);
 
 
 		// ★ 126に13を掛けた値を出力してください
+		int b;
+		b=126*13;
+		System.out.println(b);
 
 
 		// ★ 97を6で割ったときの余りを出力してください
+		int c;
+		c=97%6;
+		System.out.println(c);
 
 
 	}
