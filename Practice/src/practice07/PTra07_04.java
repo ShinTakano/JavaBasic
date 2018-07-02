12d385f9c6bd8a23d619486e8e1e4dcffcb1601f
user-PC\user <believe.3785@gmail.com>
2018-07-02 16:51:46 +0900

    7-6

--- Practice/src/practice07/PTra07_04.java
+++ Practice/src/practice07/PTra07_04.java
@@ -10,15 +10,22 @@ public class PTra07_04 {
 	public static void main(String[] args) {
 
 		// ★ int型の配列trumpを宣言し、52個の領域を確保してください
+		int[] trump;
+		trump = new int[52];
 
 
-		// ★ 配列trumpに、[1,2,3,4,5,・・・・,52] を代入してください
+		// ★ 配列trumpに、[1,2,3,4,5,・・・・,52] を代入してください→trumpの中身
+		for(int i = 0;i < trump.length;i++) {
+			trump[i] = i+1;
+		}
 
 
-		// ★ 0～51の中からランダムで値を取得して、変数indexに代入してください
+		// ★ 0～51の中からランダムで値を取得して、変数indexに代入してください→trumpの中をランダムに表示
+		int index = new java.util.Random().nextInt(trump.length);
 
 
 		// ★ 配列trumpのindex番目の中に入っている数字を出力してください
+		System.out.println(trump[index]);
 
 	}
 }
